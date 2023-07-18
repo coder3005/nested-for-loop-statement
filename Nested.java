a8a85b41cfe4624ecc9eb23932b1c447fb8d3a3f
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-18 19:54:21 +0530

    Create Nested.java

---
+++ Nested.java
@@ -0,0 +1,17 @@
+/* Nested For Loop Example..! */
+class Nested
+{
+    public static void main(String[] args) {
+
+        int i,j;
+
+        for (i=1;i<=5;i++)  //row
+        {
+            for (j=1;j<=5;j++)  //column
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
